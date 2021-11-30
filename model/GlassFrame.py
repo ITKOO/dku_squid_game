62f615e7a80dafdfd4ad5f2c4858d7bce2896254
ITKOO <itkoo2000@dankook.ac.kr>
2021-12-01 00:38:52 +0900

    유리 다리 게임 로직 수정

--- model/GlassFrame.py
+++ model/GlassFrame.py
@@ -5,58 +5,63 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from constant import Color
 from model import SGFrame
 from model import FailedFrame
+from model import RankingFrame
+
 
 class GlassFrame(SGFrame.SGFrame):
+    clickCount = 0
     def __init__(self, parent):
         super(GlassFrame, self).__init__(parent)
         self.glass()
 
     def fail(self, event):  # 유리 사다리 게임 실패시 실패 화면이 나오게 하는 함수 정의
+       self.removeGlassElement()
        self.failedFrame = FailedFrame.FailedFrame(self)
 
-    def goal(self, event):  # 게임 성공시 나오는 goal 함수 정의
-        end = Button(None, text="골인")
-        end.pack()
-        end.bind("<Button-1>", self.goal)
-        end.place(x=625, y=30)
-        photo = PhotoImage(file="../img/glass/오징어게임 성공.png")
-        w = Label(image=photo)
-        w.photo = photo
-        w.pack()
+    def success(self, event, buttonElement):
+        print('성공')
+        self.clickCount += 1
+        buttonElement.destroy()
+
+        if self.clickCount >= 5:
+            self.goal()
+
+    def goal(self):  # 게임 성공시 나오는 goal 함수 정의
+        self.removeGlassElement()
+        self.rankingFrame = RankingFrame.RankingFrame(self)
 
     def glass(self):  # 유리 사다리 게임 실행 화면
         photo = PhotoImage(file="../img/glass/오징어게임 유리다리.png")
-        w = Label(image=photo)
-        w.photo = photo
-        w.pack()
+        self.background = Label(image=photo, width=1280, height=800)
+        self.background.place(x=0, y=100)
+        self.background.photo = photo
+        self.background.pack()
 
-        b1 = Button(text=" ", height=5, width=20)
-        b1["bg"] = "white"
-        b1.pack()
-        b1.place(x=480, y=70)
-        b1.bind("<Button-1>", self.goal)
+        randomXList = [400, 630]
+        for i in range(1, 6):
+            randomX1 = randomXList[0]
+            randomX2 = randomXList[1]
+            randomNum = random.randint(1, 100)
 
-        b2 = Button(text=" ", height=5, width=20)
-        b2["bg"] = "white"
-        b2.pack()
-        b2.place(x=670, y=70)
-        b2.bind("<Button-1>", self.goal)
+            if(randomNum % 2 == 0): # 짝수면 오른쪽
+                randomX1 = randomXList[1]
+                randomX2 = randomXList[0]
 
-        for t in range(3, 11, 2):  # 반복문 사용
-            for i in range(3, 11, 2):
-                bi = Button(text=" ", height=5, width=5 * (i + 3))  # 홀수번 버튼 생성
-                bi["bg"] = "white"
-                bi.pack()
-                bi.place(x=510 - (i * 30), y=60 + (i - 1) * 70)
+            self.setSuccessButton(randomX1, 100 * i)
+            self.setFailButton(randomX2, 100 * i)
 
-                bt = Button(text=" ", height=5, width=5 * (i + 3))  # 짝수번 버튼 생성
-                bt["bg"] = "white"
-                bt.pack()
-                bt.place(x=670, y=60 + (i - 1) * 70)
 
-                k = random.randint(1, 2)  # 랜덤을 사용하여 홀수,짝수 버튼 중 탈락 버튼 결정
-                if k == 1:
-                    bi.bind("<Button-1>", self.fail)
+    def setSuccessButton(self, xCoordinate, yCoordinate):
+        print('성공 ', '(', str(xCoordinate), ',', str(yCoordinate), ')')
+        button1 = Button(self.background, text="성공", height=5, width=20, bg='white')
+        button1.place(x=xCoordinate, y=yCoordinate)
+        button1.bind("<Button>", lambda event, buttonElement=button1: self.success(event, buttonElement))
 
-                else:
-                    bt.bind("<Button-1>", self.fail)
+    def setFailButton(self, xCoordinate, yCoordinate):
+        print('실패 ', '(', str(xCoordinate), ',', str(yCoordinate), ')')
+        button1 = Button(self.background, text=" ", height=5, width=20, bg='white')
+        button1.place(x=xCoordinate, y=yCoordinate)
+        button1.bind('<Button>', self.fail)
+
+    def removeGlassElement(self):
+        self.background.destroy()
