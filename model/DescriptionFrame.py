41fa0ffb0b869c7a29c8f744f15394aa2825416d
chaerinLee21 <dlcofls0105@gmail.com>
2021-11-30 23:23:22 +0900

    홈화면 및 설명부분 개발

--- model/DescriptionFrame.py
+++ model/DescriptionFrame.py
@@ -35,21 +35,39 @@ class DescriptionFrame(SGFrame.SGFrame):
         self.exitButton.bind('<Button>', self.moveHome)
 
     def moveCharacterFrame(self, event):
+        self.photo = PhotoImage(file="../img/description/캐릭터.png")
+        self.w = Label(image=self.photo)
+        self.w.photo = self.photo
+        self.w.pack()
+        self.w.place(x=1, y=1)
+        
         self.description.config(text='게임을 진행할 캐릭터를 선택해주세요')
         self.description.place(x=470, y=160)
         self.exitButton.destroy()
         self.nextButton.destroy()
 
-    def moveSugarDescriptionFrame(self, event):
-        self.description.config(text='반갑습니다. 000 참가자\n\n첫번째 게임은 설탕뽑기입니다.\n\n\n모양은 랜덤으로 선택됩니다.')
+        self.b1 = Button(text="", bg="white", height=6, width=20)
+        self.b1.place(x=200, y=450)
+        self.b1.bind("<Button>", lambda event, tshirtNumber='001': self.moveSugarDescriptionFrame(event, tshirtNumber))
+        self.b2 = Button(text="", bg="white", height=6, width=20)
+        self.b2.place(x=570, y=450)
+        self.b2.bind("<Button>", lambda event, tshirtNumber='067': self.moveSugarDescriptionFrame(event, tshirtNumber))
+        self.b3 = Button(text="", bg="white", height=6, width=20)
+        self.b3.place(x=940, y=450)
+        self.b3.bind("<Button>", lambda event, tshirtNumber='456': self.moveSugarDescriptionFrame(event, tshirtNumber))
+
+    def moveSugarDescriptionFrame(self, event, tshirtNumber):
+        self.removeTshirtsButton()
+        self.description.config(text='반갑습니다. ' + tshirtNumber + ' 참가자\n\n첫번째 게임은 설탕뽑기입니다.\n\n\n모양은 랜덤으로 선택됩니다.')
         self.description.place(x=530, y=230)
         self.nextButton.destroy()
+        self.after(2000,self.moveSugar)
 
     def moveHome(self, event):
         self.removeDescriptionElement()
         self.homeFrame = HomeFrame.HomeFrame(self)
 
-    def moveSugar(self, event):
+    def moveSugar(self):
         self.removeDescriptionElement()
         self.sugarFrame = SugarFrame.SugarFrame(self)
 
@@ -57,3 +75,10 @@ class DescriptionFrame(SGFrame.SGFrame):
         self.description.destroy()
         self.nextButton.destroy()
         self.exitButton.destroy()
+
+    def removeTshirtsButton(self):
+        self.w.destroy()
+        self.b1.destroy()
+        self.b2.destroy()
+        self.b3.destroy()
+
--- model/HomeFrame.py
+++ model/HomeFrame.py
@@ -11,13 +11,20 @@ class HomeFrame(SGFrame.SGFrame):
         self.moveHome()
 
     def moveHome(self):
+        self.photo = PhotoImage(file="../img/description/로고.png")
+        self.w = Label(image=self.photo,bd=0)
+        self.w.photo = self.photo
+        self.w.pack()
+        self.w.place(x=250,y=30)
+        
         self.startButton = Label(text='게임 시작하기', fg=Color.WHITE, bg=Color.GRAY, bd=0, font=('맑은 고딕', 25), width=20, height=2)
-        self.startButton.place(x=490, y=200)
+        self.startButton.place(x=450, y=450)
         self.startButton.bind('<Button-1>', self.startGame)
 
         self.lankingButton = Label(text='랭킹 보기', fg=Color.WHITE, bg=Color.GRAY, bd=0, font=('맑은 고딕', 25), width=20, height=2)
-        self.lankingButton.place(x=490, y=350)
+        self.lankingButton.place(x=450, y=600)
         self.lankingButton.bind('<Button>', self.viewRanking)
+
 
     def startGame(self, event):
         self.removeHomeButton()
@@ -30,3 +37,5 @@ class HomeFrame(SGFrame.SGFrame):
     def removeHomeButton(self):
         self.startButton.destroy()
         self.lankingButton.destroy()
+        self.w.destroy()
+
