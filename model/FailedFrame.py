237bc0011e74a0f384d0a4234f06448d1c691a5c
ITKOO <itkoo2000@dankook.ac.kr>
2021-11-30 23:05:33 +0900

    타이머 분리

--- model/FailedFrame.py
+++ model/FailedFrame.py
@@ -7,4 +7,6 @@ from model import SGFrame
 class FailedFrame(SGFrame.SGFrame):
     def __init__(self, parent):
         super(FailedFrame, self).__init__(parent)
-        self.label = Label(text='실패', fg=Color.WHITE, font=('맑은 고딕', 100)).place(x=530, y=250)
+        self.label = Label(text='탈락입니다.\n\n당신은 오징어 입니다.', fg=Color.WHITE,
+                           bg=Color.BLACK, font=('맑은 고딕', 25), width=40, height=10)
+        self.label.place(x=300, y=160)
--- model/SGWindow.py
+++ model/SGWindow.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import time
 
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -18,24 +17,9 @@ class SGWindow():
         # 2. 오른쪽 사이드 상단 작은 로고 삽입
         self.setSideLogoImage()
 
-        # 3. 타이머
-        if isTimerShow:
-            self.timerLabel = Label(
-                fg=Color.MAIN,
-                bg=Color.BLACK,
-                font=('맑은 고딕', 35))
-            self.timerLabel.pack()
-            self.setTimer()
-
     def setSideLogoImage(self):
         self.smallLogoImage = PhotoImage(file="../img/sugar/small_logo.png")
         self.sideLogoLabel = Label(self.root,
                                    image=self.smallLogoImage,
                                    bd=0)
         self.sideLogoLabel.place(x=1100, y=30)
-
-    def setTimer(self):
-        now = time.strftime("%M:%S")
-        self.timerLabel.configure(text=now)
-        self.timerLabel.place(x=50, y=30)
-        self.root.after(1000, self.setTimer)
